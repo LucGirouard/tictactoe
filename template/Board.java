c70136aa96c17e885d603f41a9fe629a04208cc2
olivier0922 <olivier.michaud.2@ens.etsmtl.ca>
2026-02-01 09:43:32 -0500

    Implémentation board.java

--- template/Board.java
+++ template/Board.java
@@ -10,7 +10,12 @@ class Board
 
     // Ne pas changer la signature de cette méthode
     public Board() {
-
+        board = new Mark[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = Mark.EMPTY;
+            }
+        }
     }
 
     // Place la pièce 'mark' sur le plateau, à la
@@ -18,8 +23,9 @@ class Board
     //
     // Ne pas changer la signature de cette méthode
     public void play(Move m, Mark mark){
-
-
+        if (m.getRow() >= 0 && m.getRow() < 3 && m.getCol() >= 0 && m.getCol() < 3) {
+            board[m.getRow()][m.getCol()] = mark;
+        }
     }
 
 
@@ -28,6 +34,94 @@ class Board
     //           0   pour un match nul
     // Ne pas changer la signature de cette méthode
     public int evaluate(Mark mark){
+        Mark opponent = (mark == Mark.X) ? Mark.O : Mark.X;
+        
+        if (hasWon(mark)) {
+            return 100;
+        } else if (hasWon(opponent)) {
+            return -100;
+        } else {
+            return 0;
+        }
+    }
 
+    // Génère la liste des coups possibles (cases vides)
+    public ArrayList<Move> getPossibleMoves() {
+        ArrayList<Move> moves = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == Mark.EMPTY) {
+                    moves.add(new Move(i, j));
+                }
+            }
+        }
+        return moves;
+    }
+
+    // Vérifie si le joueur spécifié a gagné
+    public boolean hasWon(Mark mark) {
+        // Vérifier les lignes
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == mark && board[i][1] == mark && board[i][2] == mark) {
+                return true;
+            }
+        }
+        
+        // Vérifier les colonnes
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == mark && board[1][j] == mark && board[2][j] == mark) {
+                return true;
+            }
+        }
+        
+        // Vérifier les diagonales
+        if (board[0][0] == mark && board[1][1] == mark && board[2][2] == mark) {
+            return true;
+        }
+        if (board[0][2] == mark && board[1][1] == mark && board[2][0] == mark) {
+            return true;
+        }
+        
+        return false;
+    }
+
+    // Vérifie si le plateau est plein
+    public boolean isFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == Mark.EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Vérifie si la partie est terminée
+    public boolean isGameOver() {
+        return hasWon(Mark.X) || hasWon(Mark.O) || isFull();
+    }
+
+    // Retourne la marque à une position donnée
+    public Mark getMark(int row, int col) {
+        if (row >= 0 && row < 3 && col >= 0 && col < 3) {
+            return board[row][col];
+        }
+        return Mark.EMPTY;
+    }
+
+    // Affiche le plateau (pour debug)
+    public void display() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == Mark.EMPTY) {
+                    System.out.print(". ");
+                } else {
+                    System.out.print(board[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
